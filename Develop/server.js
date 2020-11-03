db76c77a5d5c7824dfb977e2381faa5f4a878d1d
Carla <carla@layc-dc.org>
2020-11-03 18:08:03 -0500

    added api route to delete note by id number and rewrite them to the json file

--- Develop/server.js
+++ Develop/server.js
@@ -22,7 +22,7 @@ app.get('/notes', (req, res) => {
 })
 
 //API Routes
-//GET to read `db.json` file and return all saved notes as JSON
+//GET to read `db.json` file 
 
 app.get("/api/notes", (req, res) => {
 //     //read file functionality
@@ -36,11 +36,32 @@ app.get("/api/notes", (req, res) => {
 
 //POST to receive a new note to save on request body, add it to the `db.json` file and return the new note to client
 
-// app.post("<endpoint goes here>", (req, res) => {})
+app.post("/api/notes", (req, res) => {
+    notes.push(req.body);
+    res.json(true);
+})
 
 //DELETE note by id number after it's been saved.  First, read all notes from the `db.json` file and remove the note with the given id number.  Last, rewrite the notes in the `db.json` file.
 
-// app.delete("<endpoint goes here>", (req, res) => {})
+app.delete("'api/notes/' + id", (req, res) => {
+   
+    fs.readFile('db.json', function(err, data) {
+        if (err) throw err;
+        JSON.parse(data);
+    })
+    let deleteId = id
+    console.log(deleteId);
+
+    if (deleteId === true) {
+        notes.filter(req.body);
+        notes.length = 0;
+        res.json({ ok: true });}
+
+    fs.writeFile('db.json', function(err, data) {
+        if (err) throw err;
+        JSON.parse(data);
+    })
+})
 
 //Start our server
 app.listen(PORT, function() {
