50e74b783f4ba30b7766e3d67a7faf436668e189
Carla <carla@layc-dc.org>
2020-11-08 16:11:55 -0500

    modified API routes to post and delete

--- Develop/server.js
+++ Develop/server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+const sid = require('shortid');
 var path = require("path");
 
 const PORT = 5500;
 
 //Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -25,45 +26,63 @@ app.get('/notes', (req, res) => {
 //GET to read `db.json` file 
 
 app.get("/api/notes", (req, res) => {
-//     //read file functionality
-    fs.readFile('db.json', function(err, data) {
+    //     //read file functionality
+    fs.readFile('./db/db.json', function (err, data) {
         if (err) throw err;
         JSON.parse(data);
+
+        const notes = JSON.parse(data);
+        res.json(notes);
     })
-//     //respond with json
-    res.json(noteList)
- })
+})
 
 //POST to receive a new note to save on request body, add it to the `db.json` file and return the new note to client
 
 app.post("/api/notes", (req, res) => {
-    notes.push(req.body);
-    res.json(true);
+    fs.readFile('.db/db.json', function (err, data) {
+        if (err) throw err;
+        JSON.parse(data);
+
+        const notes = JSON.parse(data);
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: sid.generate()
+        }
+
+        notes.push(newNote);
+        fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(notes, null, 2), function (err) {
+            if (err) throw new Error(err);
+            res.send("Note successfully saved!");
+        });
+    })
 })
 
 //DELETE note by id number after it's been saved.  First, read all notes from the `db.json` file and remove the note with the given id number.  Last, rewrite the notes in the `db.json` file.
 
 app.delete("'api/notes/' + id", (req, res) => {
-   
-    fs.readFile('db.json', function(err, data) {
-        if (err) throw err;
-        JSON.parse(data);
-    })
-    let deleteId = id
-    console.log(deleteId);
 
-    if (deleteId === true) {
-        notes.filter(req.body);
-        notes.length = 0;
-        res.json({ ok: true });}
-
-    fs.writeFile('db.json', function(err, data) {
+    fs.readFile('db.json', function (err, data) {
         if (err) throw err;
-        JSON.parse(data);
+        const dbobject = JSON.parse(data);
+        const idremove = req.params.id;
+        for (let i = 0; i < dbobject.length; i++) {
+            if (dbobject[i].id.toString() === idremove) {
+                dbobject.splice(i, 1);
+            } else {
+                console.log("No matching id");
+            }
+        }
+
+        fs.writeFile('db.json', JSON.stringify(dbobject, null, 2), function (err, data) {
+            if (err) throw err;
+            res.send(`Note ${idremove} has been removed!`)
+        })
+
     })
 })
 
 //Start our server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`App now listening on port ${PORT}`)
 })
